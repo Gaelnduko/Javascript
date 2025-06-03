d5b75631d772d574eb4f796eb99ac0488eb40218
gailnduko <ndukogail190@gmail.com>
2025-06-03 14:45:07 +0300

    This is declaring a variable

---
+++ Variables.js
@@ -0,0 +1,13 @@
+
+let size; // 14 ,13,10
+const age; //19 ,17
+let temperature; //0 ,9 
+let name; //gael , stacy,landry
+let car; // porshe ,mazda
+ 
+//writing variables
+console.log (size); 
+console.log (age); 
+// console.log (cars); 
+console.log(temperature); 
+console.log (name); 
