62a119ee77dbcb82feaf8e18fe63747b19686044
gailnduko <ndukogail190@gmail.com>
2025-06-03 15:06:00 +0300

    everything ive donr

--- Variables.js
+++ Variables.js
@@ -1,13 +1,25 @@
 
 let size; // 14 ,13,10
-const age; //19 ,17
+const age = 0; //19 ,17
 let temperature; //0 ,9 
 let name; //gael , stacy,landry
 let car; // porshe ,mazda
+
+//var initialization
+size = "15";
+car = "Demio";
+
+console.log ("This is my personal project and im selling calls")
  
+console. age(12)
+console.temperature(9)
+console.name(landry)
+
+
 //writing variables
 console.log (size); 
 console.log (age); 
 // console.log (cars); 
-console.log(temperature); 
+console.log (temperature); 
 console.log (name); 
+
